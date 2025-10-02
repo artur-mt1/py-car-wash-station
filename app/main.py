9d2d35e5d2d76f697c87584a0bfaafe0a6acfc82
Artur <arturvovk780@gmail.com>
2025-10-02 22:46:22 +0300

    fix project

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class Car:
 
 class CarWashStation:
     def __init__(
-            self, distance_from_city_center: int,
+            self, distance_from_city_center: float,
             clean_power: int,
             average_rating: float,
             count_of_ratings: int
@@ -21,18 +21,18 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> int:
+    def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-        return income
+        return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            car.comfort_class * (self.clean_power - car.clean_mark)
-            * self.average_rating / self.distance_from_city_center, 1
+            car.comfort_class * (self.clean_power - car.clean_mark) *
+            self.average_rating / self.distance_from_city_center, 1
         )
 
     def wash_single_car(self, car: Car) -> None:
@@ -41,8 +41,8 @@ class CarWashStation:
 
     def rate_service(self, only_rate: int) -> float:
         self.average_rating = round(
-            (self.average_rating * self.count_of_ratings + only_rate)
-            / (self.count_of_ratings + 1), 1
+            (self.average_rating * self.count_of_ratings + only_rate) /
+            (self.count_of_ratings + 1), 1
         )
         self.count_of_ratings += 1
         return self.average_rating
